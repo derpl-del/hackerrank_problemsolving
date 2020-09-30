f6cac4fdb4553a04b8df5225044ff5487827bfeb
unknown <gilsptr@gmail.com>
2020-09-30 13:48:34 +0700

    30/09/2020

---
+++ 32.Beautiful Days at the Movies/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Complete the beautifulDays function below.
+func beautifulDays(i int32, j int32, k int32) int32 {
+	var (
+		v1  int32
+		v2  int32
+		c   int32
+		sum float64
+	)
+	for i <= j {
+		v1 = i
+		v2 = reverse(i)
+		sum = math.Abs(float64(v1-v2)) / float64(k)
+		v3 := float64(int64(sum))
+		if sum == v3 {
+			c++
+		} else {
+		}
+		i++
+	}
+	return c
+
+}
+
+func reverse(x int32) int32 {
+	s := strconv.Itoa(int(x))
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < j; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	i, _ := strconv.Atoi(string(r))
+	return int32(i)
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 1024*1024)
+
+	ijk := strings.Split(readLine(reader), " ")
+
+	iTemp, err := strconv.ParseInt(ijk[0], 10, 64)
+	checkError(err)
+	i := int32(iTemp)
+
+	jTemp, err := strconv.ParseInt(ijk[1], 10, 64)
+	checkError(err)
+	j := int32(jTemp)
+
+	kTemp, err := strconv.ParseInt(ijk[2], 10, 64)
+	checkError(err)
+	k := int32(kTemp)
+
+	result := beautifulDays(i, j, k)
+
+	fmt.Fprintf(writer, "%d\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
